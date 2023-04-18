5c2079d1a6a4f4e5b2db6e1800e813adc9daaadd
Feddyz <69622868+Feddyz@users.noreply.github.com>
2023-04-18 20:19:17 +0800

    Update CoffeeBrewer.java

--- GourmetCoffeeSystem/src/base/CoffeeBrewer.java
+++ GourmetCoffeeSystem/src/base/CoffeeBrewer.java
@@ -1,4 +1,30 @@
 package base;
 
-public class CoffeeBrewer {
+
+
+public class CoffeeBrewer extends CoffeeAccessory{
+    static String model = "";
+    static String waterSupply = "";
+    static int capacity =0;
+    //get
+    public static String getModel(){
+        return model;
+    }
+    public static String getWaterSupply(){
+        return waterSupply;
+    }
+    public static int getCapacity(){
+        return capacity;
+    }
+    //set
+    public static void setModel(String newModel){
+        model=newModel;
+    }
+    public static void setWaterSupply(String newWaterSupply){
+        waterSupply = newWaterSupply;
+    }
+    public static void setCapacity(int newCapacity){
+        capacity= newCapacity;
+    }
 }
+
