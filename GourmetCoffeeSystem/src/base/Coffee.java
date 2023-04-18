173346f48d59ec0f453d42e54fc0f182e39eba9c
Feddyz <69622868+Feddyz@users.noreply.github.com>
2023-04-18 20:18:09 +0800

    Update Coffee.java

--- GourmetCoffeeSystem/src/base/Coffee.java
+++ GourmetCoffeeSystem/src/base/Coffee.java
@@ -1,4 +1,61 @@
 package base;
 
-public class Coffee {
-        }
+
+public class Coffee extends CoffeeAccessory {
+    static String countryoforigin = "";
+    static String roast = "";
+    static String flavor = "";
+    static String aroma = "";
+    static String acidity = "";
+    static String body = "";
+    static String smallCharge ="";
+//get
+    public static String getOrigin(){
+        return countryoforigin;
+    }
+    public static String getRoast(){
+        return roast;
+    }
+    public static String getFlavor(){
+        return flavor;
+    }
+    public static String getSmallCharge(){
+        return smallCharge;
+    }
+    public static String getAroma(){
+        return aroma;
+    }
+    public static String getAcidity(){
+        return acidity;
+    }
+    public static String getBody(){
+        return body;
+    }
+    //set
+    public static void setOrigin(String newOrigin){
+        countryoforigin = newOrigin;
+    }
+    public static void setRoast(String newRoast){
+        roast = newRoast;
+    }
+    public static void setFlavor(String newFlavour){
+        flavor= newFlavour;
+    }
+    public static void setSmallCharge(String newSmallCharge){
+        smallCharge = newSmallCharge;
+    }
+    public static void setAroma(String newAroma){
+        aroma = newAroma;
+    }
+    public static void setAcidity(String newAcidity){
+        acidity = newAcidity;
+    }
+    public static void setBody(String newBody){
+        body = newBody;
+        ;
+    }
+
+
+
+
+}
