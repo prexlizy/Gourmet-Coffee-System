624575b0de516b74fc6b583095972dc9a8ec9828
ThekinkA <110724495+ThekinkA@users.noreply.github.com>
2023-04-21 14:25:33 +0800

    Update Coffee.java

--- GourmetCoffeeSystem/src/base/Coffee.java
+++ GourmetCoffeeSystem/src/base/Coffee.java
@@ -1,17 +1,28 @@
 package base;
 
-
 public class Coffee extends CoffeeAccessory {
-    static String countryoforigin = "";
+    static String origin = "";
     static String roast = "";
     static String flavor = "";
     static String aroma = "";
     static String acidity = "";
     static String body = "";
     static String smallCharge ="";
-//get
+
+    public Coffee(String inicode, String inidescription, Double iniprice,String iniorigin,String iniroast,String iniflavor,String iniaroma,String iniacidity,String inibody,String inismallCharge) {
+        super(inicode, inidescription, iniprice);
+        this.origin=iniorigin;
+        this.roast=iniroast;
+        this.flavor=iniflavor;
+        this.aroma=iniaroma;
+        this.acidity=iniacidity;
+        this.body=inibody;
+        this.smallCharge=inismallCharge;
+    }
+
+    //get
     public static String getOrigin(){
-        return countryoforigin;
+        return origin;
     }
     public static String getRoast(){
         return roast;
@@ -33,7 +44,7 @@ public class Coffee extends CoffeeAccessory {
     }
     //set
     public static void setOrigin(String newOrigin){
-        countryoforigin = newOrigin;
+        origin = newOrigin;
     }
     public static void setRoast(String newRoast){
         roast = newRoast;
@@ -52,10 +63,10 @@ public class Coffee extends CoffeeAccessory {
     }
     public static void setBody(String newBody){
         body = newBody;
-        ;
     }
 
-
-
-
+    @Override
+    public String toString() {
+        return super.toString()+"_"+getOrigin()+"_"+getRoast()+"_"+getFlavor()+"_"+getSmallCharge()+"_"+getAroma()+"_"+getAcidity()+"_"+getBody();
+    }
 }
