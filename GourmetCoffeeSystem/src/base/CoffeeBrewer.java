3ae9697b630620dbf8878df58cd9f2d32ab21d17
ThekinkA <110724495+ThekinkA@users.noreply.github.com>
2023-04-21 14:29:15 +0800

    Update CoffeeBrewer.java

--- GourmetCoffeeSystem/src/base/CoffeeBrewer.java
+++ GourmetCoffeeSystem/src/base/CoffeeBrewer.java
@@ -1,11 +1,17 @@
 package base;
 
-
-
 public class CoffeeBrewer extends CoffeeAccessory{
     static String model = "";
     static String waterSupply = "";
     static int capacity =0;
+
+    public CoffeeBrewer(String inicode, String inidescription, Double iniprice,String inimodel,String iniwaterSupply,int inicapacity) {
+        super(inicode, inidescription, iniprice);
+        this.model=inimodel;
+        this.waterSupply=iniwaterSupply;
+        this.capacity=inicapacity;
+    }
+
     //get
     public static String getModel(){
         return model;
@@ -26,5 +32,9 @@ public class CoffeeBrewer extends CoffeeAccessory{
     public static void setCapacity(int newCapacity){
         capacity= newCapacity;
     }
-}
 
+    @Override
+    public String toString() {
+        return super.toString()+getModel()+"_"+getWaterSupply()+"_"+getCapacity();
+    }
+}
